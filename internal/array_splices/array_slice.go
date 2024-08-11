7454d69eaff5722edd58b505fb2193748f9cf14a
Edvaldo Valente <edvaldovalente@outlook.com>
2024-08-11 13:12:49 +0100

    feat: sum some slices and empty slices

--- internal/array_splices/array_slice.go
+++ internal/array_splices/array_slice.go
@@ -7,3 +7,27 @@ func Sum(numbers []int) int {
 	}
 	return result
 }
+
+func SumAll(numbersSlices ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersSlices {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllRest(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			end := numbers[1:]
+			sums = append(sums, Sum(end))
+		}
+	}
+
+	return sums
+}
