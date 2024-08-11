b5c577471b417fdb83a0aa327e32e8cf47d6d548
Edvaldo Valente <edvaldovalente@outlook.com>
2024-08-11 15:55:21 +0100

    feat: struct, func and interface refactor

--- internal/struct_func_interface/struct_func_interface.go
+++ internal/struct_func_interface/struct_func_interface.go
@@ -2,7 +2,7 @@ package struct_func_interface
 
 import "math"
 
-type Forma interface {
+type Shape interface {
 	Area() float64
 }
 
@@ -15,12 +15,21 @@ type Circle struct {
 	ray float64
 }
 
+type Trianglw struct {
+	base   float64
+	height float64
+}
+
 func (r Rectangle) Parimeter() float64 {
 	return 2 * (r.height + r.width)
 }
 
 func (r Rectangle) Area() float64 {
 	return r.height * r.width
+}
+
+func (t Trianglw) Area() float64 {
+	return t.height * t.base * 0.5
 }
 
 func (c Circle) Parimeter() float64 {
