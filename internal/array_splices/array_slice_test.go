7a72e6d86d51fb3fa4fa5eafdf3c85bfe0eddca4
Edvaldo Valente <edvaldovalente@outlook.com>
2024-08-11 13:12:13 +0100

    test: sum some slices and empty slices

--- internal/array_splices/array_slice_test.go
+++ internal/array_splices/array_slice_test.go
@@ -1,6 +1,9 @@
 package array_slice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -33,6 +36,48 @@ func TestSum(t *testing.T) {
 
 		//Assert
 		verifyResult(t, result, expected, numbers)
+	})
+
+	t.Run("sum all from slices", func(t *testing.T) {
+		arrange := SumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{0, 9}
+
+		if reflect.DeepEqual(arrange, expected) {
+			t.Errorf("result '%d', expected '%d'", arrange, expected)
+		}
+	})
+
+	t.Run("", func(t *testing.T) {
+		result := SumAllRest([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v, expected %v", result, expected)
+		}
+	})
+
+}
+
+func TestSumAllRest(t *testing.T) {
+
+	verifysums := func(t *testing.T, result, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v, expected %v", result, expected)
+		}
+	}
+	t.Run("Sum some slices", func(t *testing.T) {
+		result := SumAllRest([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		verifysums(t, result, expected)
+	})
+
+	t.Run("Sum some empty slices", func(t *testing.T) {
+		result := SumAllRest([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+
+		verifysums(t, result, expected)
 	})
 
 }
